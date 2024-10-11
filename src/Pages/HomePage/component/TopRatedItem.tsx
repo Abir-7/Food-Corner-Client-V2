6c74c29798e58c4cd2c801d1e9b77fe4f42edaec
unknown <md.tazwarul.islam.07@gmail.com>
2024-10-11 20:13:13 +0600

    fix issu in top rated menu in hompage

--- src/Pages/HomePage/component/TopRatedItem.tsx
+++ src/Pages/HomePage/component/TopRatedItem.tsx
@@ -22,48 +22,11 @@ export const TopRatedItem = () => {
                 <div className="w-full absolute h-1/2  rounded-b-xl bg-orange-400 bg-opacity-90 bottom-0 group-hover:h-full duration-500"></div>
 
                 <div className="w-[200px]  rounded-3xl h-[200px] hover: bg-red-500 relative">
-                  <img className="rounded-3xl" src={item.photo} alt="" />
-                </div>
-              </div>
-
-              {/* Rating and text section */}
-              <div className=" p-4">
-                <Rating readOnly style={{ maxWidth: 100 }} value={4}></Rating>
-                <Link
-                  to={`/food-item/${item._id}`}
-                  className="text-xl font-semibold hover:text-orange-400 duration-300"
-                >
-                  {item.title}
-                </Link>
-                <p>
-                  {" "}
-                  {item?.description.length > 45
-                    ? item.description.slice(0, 45) + "..."
-                    : item?.description}
-                </p>
-
-                <div className="flex justify-between mt-1">
-                  <p className="font-bold text-green-500 ">
-                    {item.price[0].price}{" "}
-                    <span className="text-orange-400">Tk</span>
-                  </p>
-                  <p className="font-bold text-green-500 ">
-                    {item.price[0].size}{" "}
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
-          {data?.map((item) => (
-            <div
-              key={item._id}
-              className="w-72 shadow border h-[450px] pb-2 rounded-xl overflow-hidden group hover:shadow-lg duration-300"
-            >
-              <div className="w-full h-2/3 relative flex justify-center items-center rounded-xl overflow-hidden">
-                <div className="w-full absolute h-1/2  rounded-b-xl bg-orange-400 bg-opacity-90 bottom-0 group-hover:h-full duration-500"></div>
-
-                <div className="w-[200px]  rounded-3xl h-[200px] hover: bg-red-500 relative">
-                  <img className="rounded-3xl" src={item.photo} alt="" />
+                  <img
+                    className="w-full h-full object-cover rounded-3xl"
+                    src={item.photo}
+                    alt=""
+                  />
                 </div>
               </div>
 
